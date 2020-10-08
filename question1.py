8ca10ffbe01ed45f3e91caf9df005170bec228cd
Victor Lopez <victor.lopez@daviestech.org>
2020-10-08 13:47:39 +0000

    Done

---
+++ question1.py
@@ -0,0 +1,5 @@
+x = int(input("Give me a number:"))
+print(x)
+print(x+1)
+print(x+2)
+print(x+3)
---
+++ question2.py
@@ -0,0 +1,4 @@
+dec1 = float(input("Give me a decimal:"))
+dec2 = float(input("Give me a decimal:"))
+dec3 = float(input("Give me a decimal:"))
+print (dec1 + dec2 + dec3)
